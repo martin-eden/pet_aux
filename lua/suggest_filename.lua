9c67dff9eb74d053751fe067dffa3b19cd31c19b
Ian <ian@somewhere.com>
2016-03-22 07:32:33 +0100

    [^] [suggest_filename] Code beautification and linking to library hive.

--- lua/suggest_filename.lua
+++ lua/suggest_filename.lua
@@ -1,44 +1,46 @@
+-- Extracts download link and gives a neat file name for given JSON file
+-- for server.
+
 --[[
-  Extracts download link and gives a neat file name for given JSON file
-  for server.
+  Status: done
+  Last mod.: 2016-03-22
+  Notes:
 
-  Usage: <json_auction_link>
+    Usage: <json_auction_link>
+
+    <json_auction_link> sample contents:
+
+    {
+      "files": [
+        {
+          "url": "http://auction-api-eu.worldofwarcraft.com/auction-data/b6a53e4bbf5d5f7b78b69a419b5d4970/auctions.json",
+          "lastModified": 1454101069000
+        }
+      ]
+    }
+
+    Indeed JSON is minified.
 ]]
---[[
-  Auction link JSON sample:
 
-  {
-    "files": [
-      {
-        "url": "http://auction-api-eu.worldofwarcraft.com/auction-data/b6a53e4bbf5d5f7b78b69a419b5d4970/auctions.json",
-        "lastModified": 1454101069000
-      }
-    ]
-  }
+require('base_structs.#base')
 
-  Indeed JSON is minified.
-]]
+local file_as_string = request('base_structs.file.as_string')
+local json_as_table = request('base_structs.parse.json')
 
-require('common')
-
-local filename_time_format = '%04d-%02d-%02d_%02d_%02d'
-local output_format = '%s %s_%s.json'
 local json_link = arg[1]
 local servername = string.match(json_link, '.*%/([%w%-]+)') or json_link
-local auc_link = json_as_table(file_as_string(json_link))
-local parsed_time = os.date('!*t', auc_link.files[1].lastModified / 1000)
-local filetime =
-  filename_time_format:format(
-    parsed_time.year,
-    parsed_time.month,
-    parsed_time.day,
-    parsed_time.hour,
-    parsed_time.min
-  )
-result = output_format:format(auc_link.files[1].url, servername, filetime)
+local auc_link_data = json_as_table(file_as_string(json_link))
+
+local snapshot_time = auc_link_data.files[1].lastModified / 1000
+local snapshot_path = auc_link_data.files[1].url
+
+local filetime = os.date('%Y-%m-%dT%H_%M', snapshot_time)
+
+local result = ('%s %s_%s.json'):format(snapshot_path, servername, filetime)
 print(result)
 
 --[[
 ~ 2013-09
 2016-01-31
+2016-03-22
 ]]
---
+++ lua/test_modules_plug.lua
@@ -0,0 +1,6 @@
+require('base_structs.#base')
+print(type(is_number))
+
+local print_table = request('base_structs.debug.print_table')
+print(type(print_table))
+-- print_table(_G)
