6bf1e7d62ccd6b919288fb70702873d25e5b0b47
Ian <ian@somewhere.com>
2017-10-22 23:45:52 +0100

    [^] Considering merged realms.
    As WoW is dying, realms are merged. So there is only 121 distinct
    auctions and 267 "realms". Now code considers merged realms and
    will donwload 121 file instead of 267.

--- lua/map_realms.lua
+++ lua/map_realms.lua
@@ -1,8 +1,13 @@
+--[[
+  Filter data from .json realms description.
+]]
+
 require('workshop.base')
 
 local file_as_string = request('!.file.as_string')
 local table_from_str = request('!.formats.lua_table.load')
 local table_to_str = request('!.formats.lua_table.save')
+local map_values = request('!.table.map_values')
 
 local lua_realms_list_filename = arg[1]
 local output_file_name = arg[2]
@@ -11,7 +16,11 @@ local realms_list = table_from_str(file_as_string(lua_realms_list_filename))
 local result = {}
 for i = 1, #realms_list.realms do
   local rec = realms_list.realms[i]
-  result[rec.name] = rec.slug
+  result[rec.name] =
+    {
+      slug = rec.slug,
+      connected_realms = map_values(rec.connected_realms),
+    }
 end
 
 local filtered_str = table_to_str(result)
@@ -19,3 +28,40 @@ local filtered_str = table_to_str(result)
 local f_out = assert(io.open(output_file_name, 'w'))
 f_out:write(filtered_str)
 f_out:close()
+
+
+--[[
+  Sample JSON:
+
+  {
+    "realms": [
+      {
+        "type":"pve",
+        "population":"low",
+        "queue":false,
+        "status":true,
+        "name":"Blade's Edge",
+        "slug":"blades-edge",
+        "battlegroup":"Glutsturm / Emberstorm",
+        "locale":"en_GB",
+        "timezone":"Europe/Paris",
+        "connected_realms":["blades-edge","eonar","veknilash"]
+      },
+      ...
+    ]
+  }
+
+  Produced Lua:
+
+  {
+    ["Blade's Edge"] = {
+      slug = 'blades-edge',
+      connected_realms = {
+        ['blades-edge'] = true,
+        ['eonar'] = true,
+        ['veknilash'] = true,
+      },
+    },
+    ...
+  }
+]]
--- lua/print_realm_slugs_from_config.lua
+++ lua/print_realm_slugs_from_config.lua
@@ -1,3 +1,9 @@
+--[[
+  For full realm names in [config] print list of slug names.
+
+  These list is used to download auctions.
+]]
+
 require('workshop.base')
 
 local file_as_string = request('!.file.as_string')
@@ -9,15 +15,20 @@ if not config_realms then
   error(('Looks like config file "%s" is not loaded correctly.'):format(config_file_name))
 end
 
-local realms_name = arg[2]
-local full_realms_list = table_from_str(file_as_string(realms_name))
+local realms_file_name = arg[2]
+local full_realms_list = table_from_str(file_as_string(realms_file_name))
 if not full_realms_list then
-  error(('Looks like realms file "%s" is not loaded correctly.'):format(realms_name))
+  error(('Looks like realms file "%s" is not loaded correctly.'):format(realms_file_name))
 end
 
-for i = 1, #config_realms do
-  local realm_name = config_realms[i]
-  if full_realms_list[realm_name] then
-    print(full_realms_list[realm_name])
+local realms_processed = {}
+for _, realm_name in ipairs(config_realms) do
+  local realm_rec = full_realms_list[realm_name]
+  -- If config has correct full realm name and it is not previously mentioned:
+  if realm_rec and not realms_processed[realm_rec.slug] then
+    print(realm_rec.slug)
+    for k in pairs(realm_rec.connected_realms) do
+      realms_processed[k] = true
+    end
   end
 end
